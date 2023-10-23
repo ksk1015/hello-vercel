32470e8c9679ee43611232780d667c40530d18f0
ksk1015 <ksk.aizawa@gmail.com>
2023-10-23 11:40:29 +0900

    gnav use clinet に戻す

--- app/_components/HeaderNavigation.tsx
+++ app/_components/HeaderNavigation.tsx
@@ -1,10 +1,10 @@
-// 'use client'
+'use client'
 
-// import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 export default function HeaderNavigation() {
-  // const pathname = usePathname()
+  const pathname = usePathname()
   const links = [
     { href: '/', title: 'Home' },
     { href: '/hello', title: 'Hello' },
@@ -17,14 +17,14 @@ export default function HeaderNavigation() {
           <Link
             key={i}
             href={item.href}
-            // aria-current={item.href === pathname ? 'page' : undefined}
+            aria-current={item.href === pathname ? 'page' : undefined}
             className="text-blue-700 underline aria-[current]:text-black aria-[current]:no-underline"
           >
             {item.title}
           </Link>
         ))}
       </nav>
-      {/* <div className="mt-2 text-center text-gray-500 text-sm">{pathname}</div> */}
+      <div className="mt-2 text-center text-gray-500 text-sm">{pathname}</div>
     </div>
   )
 }
